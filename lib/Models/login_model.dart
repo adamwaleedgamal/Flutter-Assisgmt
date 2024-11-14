e38005c49aea6477d5543b1676dd524011992d22
adamwaleedgamal <151035865+adamwaleedgamal@users.noreply.github.com>
2024-11-14 10:07:39 +0200

    Update login_model.dart

--- lib/Models/login_model.dart
+++ lib/Models/login_model.dart
@@ -2,6 +2,7 @@ class LoginModel {
   Map<String, dynamic> data;
   bool status;
   String message;
+  //last
   LoginModel({required this.data, required this.message, required this.status});
 
   factory LoginModel.fromJson(Map<String, dynamic> json) {
